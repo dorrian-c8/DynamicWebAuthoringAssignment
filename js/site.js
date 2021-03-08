ed82063898710d0f8d30fdd8a12d5696140aeedf
dorrian-c8 <56315585+dorrian-c8@users.noreply.github.com>
2021-03-08 15:23:02 +0000

    Remove old pages and add housepal styles

--- js/site.js
+++ js/site.js
@@ -2,23 +2,41 @@ $(function () {
     drawNavBar();
 });
 
+// Call this so same nav is present on all pages
 function drawNavBar(){
     var navbar = '';
-    navbar += '<nav class="navbar navbar-expand-lg navbar-light bg-light">';
-    navbar += '<a class="navbar-brand" href="index.html">Navbar</a>';
+    navbar += '<nav class="navbar navbar-expand-lg navbar-light">';
+    navbar += '<a class="navbar-brand" href="index.html"><img src="images/Logo_header.png" alt="Housepal Logo"></a>';
     navbar += '<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">';
     navbar += '<span class="navbar-toggler-icon"></span>';
     navbar += '</button>';
     navbar += '<div class="collapse navbar-collapse" id="navbarNav">';
-    navbar += '<ul class="navbar-nav">';
+    navbar += '<ul class="navbar-nav left-nav">';
     navbar += '<li class="nav-item">';
-    navbar += '<a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>';
+    navbar += '<a class="nav-link" href="index.html">Home</a>';
     navbar += '</li>';
-    navbar += '<li class="nav-item">';
-    navbar += '<a class="nav-link" href="features.html">Features</a>';
+    navbar += '<li class="nav-item ml-4">';
+    navbar += '<a class="nav-link" href="listings.html">Listings</a>';
     navbar += '</li>';
+    navbar += '<li class="nav-item ml-4">';
+    navbar += '<a class="nav-link" href="aboutUs.html">About Us</a>';
+    navbar += '</li>';
+    navbar += '<li class="nav-item ml-4">';
+    navbar += '<a class="nav-link" href="testimonials.html">Testimonials</a>';
+    navbar += '</li>';
+    navbar += '<li class="nav-item ml-4">';
+    navbar += '<a class="nav-link" href="contact.html">Contact</a>';
+    navbar += '</li>';
+    navbar += '</ul>';
+    navbar += '<ul class="navbar-nav media-nav">';
     navbar += '<li class="nav-item">';
-    navbar += '<a class="nav-link" href="pricing.html">Pricing</a>';
+    navbar += '<a class="nav-link" href="https://en-gb.facebook.com/" target="_blank"><i class="fa fa-facebook-square fa-2x"></i></a>';
+    navbar += '</li>';
+    navbar += '<li class="nav-item ml-4">';
+    navbar += '<a class="nav-link" href="https://www.instagram.com/" target="_blank"><i class="fa fa-instagram fa-2x"></i></a>';
+    navbar += '</li>';
+    navbar += '<li class="nav-item ml-4">';
+    navbar += '<a class="nav-link" href="https://twitter.com/login?lang=en-gb" target="_blank"><i class="fa fa-twitter fa-2x"></i></a>';
     navbar += '</li>';
     navbar += '</ul>';
     navbar += '</div>';
